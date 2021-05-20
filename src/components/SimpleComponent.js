c2d6541e2302b9ada8f20f3a11790bb5c8fa1fdd
Ynda Jas <yndajas@gmail.com>
2021-05-20 03:40:53 +0100

    commit tree 1d30755681a9f4065f3d666891e7d48461e7d275

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,29 @@
 // Code SimpleComponent Here
+import React, { Component } from "react";
+
+class SimpleComponent extends Component {
+    constructor() {
+        super();
+        this.moods = {
+            happy: "sad",
+            sad: "happy"          
+        }
+        this.state = {
+            mood: "happy"
+        };
+    }
+
+    render() {
+        return (
+            <div onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        );
+    }
+
+    handleClick = () => {
+        this.setState({mood: this.moods[this.state.mood]})
+    }
+}
+
+export default SimpleComponent;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const happyAlert = () => {
+  alert("Stay happy!")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={happyAlert} />
   </div>,
   document.getElementById('root')
 );
